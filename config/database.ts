88db9e21c2b7c63d507f707201c8044e67c7a2a9
helena karol <helenadiasmaia@gmail.com>
2021-06-01 10:11:32 -0300

    Prepare to deploy

--- config/database.ts
+++ config/database.ts
@@ -8,32 +8,16 @@
 import Env from '@ioc:Adonis/Core/Env'
 import Application from '@ioc:Adonis/Core/Application'
 import { DatabaseConfig } from '@ioc:Adonis/Lucid/Database'
+import Url from 'url-parse'
+
+const databaseUrl = new Url(Env.get('DATABASE_URL'))
 
 const databaseConfig: DatabaseConfig = {
-  /*
-  |--------------------------------------------------------------------------
-  | Connection
-  |--------------------------------------------------------------------------
-  |
-  | The primary connection for making database queries across the application
-  | You can use any key from the `connections` object defined in this same
-  | file.
-  |
-  */
+  
   connection: Env.get('DB_CONNECTION'),
 
   connections: {
-    /*
-    |--------------------------------------------------------------------------
-    | SQLite
-    |--------------------------------------------------------------------------
-    |
-    | Configuration for the SQLite database.  Make sure to install the driver
-    | from npm when using this connection
-    |
-    | npm i sqlite3
-    |
-    */
+   
     sqlite: {
       client: 'sqlite',
       connection: {
@@ -43,6 +27,27 @@ const databaseConfig: DatabaseConfig = {
         naturalSort: true,
       },
       useNullAsDefault: true,
+      healthCheck: false,
+      debug: false,
+    },
+
+   
+    pg: {
+      client: 'pg',
+      connection: {
+        host: databaseUrl.hostname as string,
+        port: databaseUrl.port as number,
+        user: databaseUrl.username as string,
+        password: databaseUrl.password as string,
+        database: databaseUrl.pathname.substr(1) as string,
+        ssl: {
+          rejectUnauthorized: false
+        },
+      },
+     
+      migrations: {
+        naturalSort: true,
+      },
       healthCheck: false,
       debug: false,
     },
